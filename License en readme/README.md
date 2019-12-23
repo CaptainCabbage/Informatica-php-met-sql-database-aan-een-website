350873bf3f7a1781f798224bdb35cfdf90374f05
CaptainCabbage <15994@calvijncollege.com>
2019-12-23 10:32:54 +0000

    Netjes gemaakt en bij de meeste dingen context gezet over hoe en wat

--- License en readme/README.md
+++ License en readme/README.md
@@ -1,4 +1,5 @@
 # Informatica-php-met-sql-database-aan-een-website
 
+//Dit is de command die je van onderen in het commandcenter gedoe moet invoeren om de site te runnen, "3000" is de port waar de site wordt geopend.
 php -S localhost:3000
 
--- src/overzicht.php
+++ src/overzicht.php
@@ -1,21 +1,25 @@
 <?php
+//"$servername" is voor het ip om de database te bereiken
+//"$username" en "$password" spreken voor zich, dat zijn de inloggevens om de database binnen te komen
+//"$schema" geeft specifiek aan welke database
     $servername = "127.0.0.1";
     $username = "u392629804_usergroep3";
     $password = "E&=9HA3l";
     $schema = "u392629804_groep3";
-    // Create connection
+//Connectie aanmaken
     $conn = new mysqli($servername, $username, $password, $schema);
 
-    // Check connection
+//De connectie checken of hij werkt
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
 
+//Dit zorgt er voor dat het programma data haalt uit een bepaalde tabel, in dit geval uit de tabel "Boerenbedrijg", omdat dat staat aangegeven
     $sql = "SELECT * FROM Boerenbedrijf";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
-        // output data of each row
+// output data van elke rij 
         while($row = $result->fetch_assoc()) {
            echo "id: " . $row["bedrijf_id"]. " - Naam: " . $row["voornaam_eigenaar"]. " " . $row["naam_eigenaar"]. "<br>";
         }
